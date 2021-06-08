9766a4ed189a98d023d39c6ce75f6eba9e70bf49
galosi <galosi@iiasa.ac.at>
2021-06-08 11:12:21 +0200

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,3 +16,19 @@ resource "azurerm_resource_group" "nordcloud" {
   location = "westeurope"
 }
 
+#Create Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "nordcloud-vnet"
+  address_space       = ["192.168.0.0/16"]
+  location            = "westeurope"
+  resource_group_name = azurerm_resource_group.nordcloud.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet"
+  resource_group_name  = azurerm_resource_group.nordcloud.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefix       = "192.168.0.0/24"
+}
+  
