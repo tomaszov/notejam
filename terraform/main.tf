0dfe1e8d23b30a0a33f42f172a0d394a828905a7
galosi <galosi@iiasa.ac.at>
2021-06-08 09:23:49 +0200

    terraform

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+provider "azurerm" {
+  version = "=2.0.0"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "nordcloud"
+    storage_account_name = "notejamtf"
+    container_name       = "tfstatedevops"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_resource_group" "nordcloud" {
+  name     = "nordcloud"
+  location = "westeurope"
+}
