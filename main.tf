c20d9fcb1181a3a3c54ce7e1081b30d848195dc0
galosi <galosi@iiasa.ac.at>
2021-06-12 16:36:20 +0200

    update env

--- main.tf
+++ main.tf
@@ -86,6 +86,57 @@ resource "azurerm_app_service" "notejam" {
   app_settings = local.env_variables
 }
 
+resource "azurerm_monitor_autoscale_setting" "notejam_scaling" {
+  name                = "notejam_autoscale"
+  resource_group_name = azurerm_resource_group.nordcloud.name
+  location            = azurerm_resource_group.nordcloud.location
+  target_resource_id  = azurerm_app_service_plan.nordcloud_notejam.id
+  profile {
+    name = "default"
+    capacity {
+      default = 1
+      minimum = 1
+      maximum = 10
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.nordcloud_notejam.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "GreaterThan"
+        threshold          = 90
+      }
+      scale_action {
+        direction = "Increase"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+    rule {
+      metric_trigger {
+        metric_name        = "CpuPercentage"
+        metric_resource_id = azurerm_app_service_plan.nordcloud_notejam.id
+        time_grain         = "PT1M"
+        statistic          = "Average"
+        time_window        = "PT5M"
+        time_aggregation   = "Average"
+        operator           = "LessThan"
+        threshold          = 10
+      }
+      scale_action {
+        direction = "Decrease"
+        type      = "ChangeCount"
+        value     = "1"
+        cooldown  = "PT1M"
+      }
+    }
+  }  
+}
+
 module "mssql-server" {
   source  = "kumarvna/mssql-db/azurerm"
   version = "1.1.0"
